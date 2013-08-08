eead1197d95d42919ad829a618a37e50b439c63b
guptag <guptagaruda@gmail.com>
2013-08-08 11:07:00 -0400

    error message

--- js/views/marketdataview.js
+++ js/views/marketdataview.js
@@ -21,6 +21,7 @@ define([], function () {
 
     _this.showError = function() {
       //show error
+      $container.html("Couldn't retrieve the data at this time...Please try again later.")
     }
 
     _this.preRender = function () {
