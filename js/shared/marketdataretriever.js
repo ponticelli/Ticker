f9daa6af89afba3c3d618af2de89450d02425311
guptag <guptagaruda@gmail.com>
2013-08-08 16:27:26 -0400

    fix typo

--- js/shared/marketdataretriever.js
+++ js/shared/marketdataretriever.js
@@ -1,7 +1,7 @@
 define(['models/marketdata'], function (MarketData) {
   // Fetch the market data from service calls (singleton)
   var MarketDataRetriever = new function() {
-    var _this = this;
+    var _this = this,
         rawUrl_endPoint1 = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%3D%22{{ticker}}%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&stamp={{timestamp}}",
         rawUrl_endPoint2 = "http://ec2-54-224-143-194.compute-1.amazonaws.com/{{ticker}}?stamp={{timestamp}}";
 
